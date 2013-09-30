b8935e83dcf8c03e0732938215f5bd473154586c
mikeymckay <mikeymckay@gmail.com>
2013-09-30 15:12:02 -0400

    Adding recording

--- papaya-server.rb
+++ papaya-server.rb
@@ -1,24 +1,18 @@
 require 'rubygems'
 require 'sinatra'
 require 'rest-client'
-require 'moneta'
 require 'json'
 require 'yaml'
 require 'fileutils'
 
-# Create a simple file store
-$database = Moneta.new(:File, :dir => 'database')
-
-$tunnel_server = 'http://localhost'
-
 set :enviroment, :development
 
-post '/save/:name' do |name|
+post '/save/:language' do |language|
   puts params.inspect
 
-  dir_name = "#{Dir.pwd}/public/uploads/#{params["name"]}"
+  dir_name = "#{Dir.pwd}/public/uploads/#{params["language"]}"
   FileUtils.mkdir_p dir_name
-  File.open(File.join(dir_name, "phonemes.json"), "w") do |f| 
+  File.open(File.join(dir_name, "#{language}.json"), "w") do |f| 
     f.write(params["data"])
   end
   return "Success"
@@ -32,69 +26,50 @@ post '/upload' do
   while blk = tmpfile.read(65536)
     dir_name = "#{Dir.pwd}/public/uploads/#{params["language-name"]}"
     FileUtils.mkdir_p dir_name
-    File.open(File.join(dir_name, "#{params[:phoneme]}.mp3"), "wb") { |f| f.write(tmpfile.read) }
+    File.open(File.join(dir_name, "#{params[:phoneme]}"), "wb") { |f| f.write(tmpfile.read) }
   end
 
   redirect '/index.html'
 end
 
-
-def post_to_google(configuration,params)
-  options = {}
-  options[configuration["google_spreadsheet_phone_parameter"]] = params[:phone]
-  options[configuration["google_spreadsheet_text_parameter"]] = params[:text]
-  options[configuration["google_spreadsheet_sent_or_received_parameter"]] = params[:sent_or_received]
-  puts configuration.inspect
-  RestClient.post configuration["google_spreadsheet_url"], options
+get '/languages' do
+  content_type :json
+  (Dir.entries("#{Dir.pwd}/public/uploads")-[".",".."]).to_json
 end
 
+get '/json_package' do
+  content_type :json
+  result = {
+    "maxRecordTime" => 15000,
+    "default_language" => params["defaultLanguage"],
+    "languages" =>  {}
+  }
 
-def send_to_phone(port,params)
-  RestClient.get "#{$tunnel_server}:#{port}/sendsms", {:params => {:phone => params[:phone], :text => params[:text]}}
+  params['languages'].split(/,/).each do |language|
+    result['languages'][language] = {
+      "phonemes" =>  language_config(language)["phonemes"],
+      "voices" =>  language_config(language)["voices"]
+    }
+  end
+
+  puts language_config("English")["phonemes"]
+  puts result
+  result.to_json
 end
 
-get '/send/:port' do |port|
-  params[:sent_or_received] = "sent"
-  post_to_google($database[port], params)
-  send_to_phone(port, params)
+def language_files(language)
+  Dir.entries("#{Dir.pwd}/public/uploads")-[".",".."]
 end
 
-get '/gateway_status/:port' do |port|
+def path_to_config(language)
+  "#{Dir.pwd}/public/uploads/#{language}/#{language}.json"
+end
+
+def language_config(language)
   begin
-    RestClient.get("#{$tunnel_server}:#{port}")
-  rescue => e
-    return e.to_s
-    return "unavailable" if e.to_s.match(/Connection refused/)
-  end
-  return "available"
-end
-
-def new_port
-  puts $database["9090"]
-  while true do
-    port = rand(9999)
-    next if port < 1000 or $database.key?(port)
-    return port.to_s
+    JSON.parse( IO.read(path_to_config(language)))
+  rescue
+    {}
   end
 end
 
-get '/new/port' do
-  new_port()
-end
-
-get '/extract_configuration/:url' do |url|
-  RestClient.get(url).scan(/(entry\.\d+)/).flatten.to_json
-end
-
-get '/configuration/:port' do |port|
-  content_type 'application/json'
-  $database[port].to_json
-end
-
-post '/update_database' do
-  config_data = {}
-  "name, google_spreadsheet_url, google_spreadsheet_phone_parameter, google_spreadsheet_text_parameter, google_spreadsheet_sent_or_received_parameter".split(/, */).each do |param|
-    config_data[param] = params[param]
-  end
-  $database[params["port"]] = config_data
-end
